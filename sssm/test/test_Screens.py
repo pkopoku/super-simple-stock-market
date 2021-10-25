11fc9445fa66cadf2c0d462ad5cf5b605ab4712a
P.K <pkopoku42@gmail.com>
2021-10-25 12:30:39 +0100

    feature(test): added a test case for user input

--- sssm/test/test_Screens.py
+++ sssm/test/test_Screens.py
@@ -1,1 +1,16 @@
-#todo
+import unittest
+from unittest.mock import patch
+from models.screen.Screen import DividendYieldScreen, PeRatioScreen
+from models.screen.Screen import RecordTradeScreen
+from models.screen.Screen import VolumeWeightedStockPrice, AllShareIndexScreen
+from models.screen.Screen import ExitScreen, MainScreen, InvalidInputScreen
+from models.screen import Banners
+
+class TestScreens(unittest.TestCase):
+
+    # mock user choice
+    @patch('models.screen.Screen.MainScreen.run', return_value=1)
+    def test_main_menu_choice(self, input):
+        screen = MainScreen(Banners.MAIN_BANNER)
+        choice = screen.run()
+        self.assertEqual(screen.run(), 1)
